992c791c321d2da3b98da5c57682556dd0c2d2f5
Levert <levertco@gmail.com>
2019-07-25 16:26:56 +0300

    marked

--- user.py
+++ user.py
@@ -11,10 +11,12 @@ class User:
 # Levert
     def user_exists(self,password):
     """Levert Co
-    This enables you to enter the password you created this account with and if it exits it will show you your stored password.Levert
+    This enables you to enter the password you created this account with and if it exits it will show you your stored password.Levert\
+    
     Args:
     Password Lockers user's signup password
     Return:
+    
     boolean-the data type that has one of two possible values
     """
         if self.password == password:
